4da10a96f67df7c36de005afc4df87c3237b574a
Stuart Johnson <Stuartjohnsoncams@gmail.com>
2017-01-11 15:56:22 +0000

    Globals minor tweak

--- src/com/battlecode/Robots/Globals.java
+++ src/com/battlecode/Robots/Globals.java
@@ -7,33 +7,17 @@ public class Globals {
     public static MapLocation here;
     public static Team friendly;
     public static Team enemy;
-    public static Direction homeDir;
-    public static MapLocation centerOfAllInitialArchons;
-    public static MapLocation centerOfFriendlyInitialArchons;
-    public static MapLocation centerOfEnemyInitialArchons;
-    public static MapLocation[] getArchonNumber;
-
-    public static int numberOfInitialArchon;
 
     public static RobotInfo[] visibleEnemies = null;
     public static RobotInfo[] visibleFriendlies = null;
     public static RobotInfo[] visibleBullets = null;
     public static RobotInfo[] visibleOnTargetBullets = null;
 
+    public static double[] circlePlantInts = {0.4, 0.8, 1.2, 1.6, 2};
+
     public static void init(RobotController theRC) {
         rc = theRC;
         friendly = rc.getTeam();
         enemy = friendly.opponent();
-
-        numberOfInitialArchon = getInitialArchonLocations(friendly).length;
-
-        centerOfFriendlyInitialArchons = new MapLocation(0,0);
-        centerOfEnemyInitialArchons = new MapLocation(0,0);
-        centerOfFriendlyInitialArchons = ArchonCenter(1.0 / (double)numberOfInitialArchon, centerOfFriendlyInitialArchons);
-        centerOfEnemyInitialArchons = ArchonCenter(1.0 / (double)numberOfInitialArchon, centerOfEnemyInitialArchons);
-    }
-
-    public static MapLocation ArchonCenter(double f, MapLocation a) {
-        return new MapLocation((int)Math.round(f * a.x), (int)Math.round(f * a.y));
     }
 }
